6883f0e5dd7e921dfbf3740ff747d539fbb398f5
Alba Lopes <albasandyra@gmail.com>
2019-04-23 20:06:18 -0300

    Atualizando o repositório

--- apisuap/js/funcionalidades.js
+++ apisuap/js/funcionalidades.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
-	$.ajax({
-			headers: { 
-				"Authorization" : "JWT "+sessionStorage.getItem("token")
-			},
-			url: "https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/",
-			contentType: 'application/json',
-			dataType: 'json',
-			type: 'GET',
-			success: function (data) {
-				$("#usuario-nome_usual").html(data.vinculo.nome);
-				$("#usuario-tipo_vinculo").html(data.tipo_vinculo);
-				$("#usuario-email").html(data.email);
-			},
-			error: function(data){
-				alert("Impossível recuperar dados. Você deve fazer login!");
-				window.location.href = "login.html";
-			}
-	});
-		
-		
-	$("#botao-sair").click(function(e){
-		sessionStorage.removeItem("token");
-		window.location.href="login.html";
-	});
 
+	$("#botao-meusdados").click(function(e){
+		$.ajax({
+				headers: { 
+					"Authorization" : "JWT "+sessionStorage.getItem("token")
+				},
+				url: "https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/",
+				contentType: 'application/json',
+				dataType: 'json',
+				type: 'GET',
+				success: function (data) {
+					$("#usuario-nome_usual").html(data.vinculo.nome);
+					$("#usuario-tipo_vinculo").html(data.tipo_vinculo);
+					$("#usuario-email").html(data.email);
+				},
+				error: function(data){
+					alert("Impossível recuperar dados. Você deve fazer login!");
+					window.location.href = "login.html";
+				}
+		});
+	});
+	
+	
+	/* COLOQUE AS NOVAS FUNCIONALIDADES AQUI ABAIXO, AINDA DENTRO DO $(document).ready */
 		
 });
 
